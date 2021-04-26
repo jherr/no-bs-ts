ce5ff3c3ce5a1094ac734a816fb7252dc7f01764
Jack Herrington <jherr@pobox.com>
2021-04-26 06:30:00 -0700

    Adding episode 5

---
+++ episode-5/functions-test.ts
@@ -0,0 +1,7 @@
+import addNumbers, { addStrings, getName } from "./functions";
+
+console.log(addNumbers(1, 2));
+console.log(addStrings("a", "b"));
+console.log(addStrings("a"));
+
+console.log(getName({ first: "jack", last: "herrington" }));
