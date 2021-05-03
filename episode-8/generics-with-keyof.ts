40b830795f12fe0c6b7f407467a7ff0b2e8450d9
Jack Herrington <jherr@pobox.com>
2021-05-03 06:09:50 -0700

    Episodes 8 and 9

---
+++ episode-8/generics-with-keyof.ts
@@ -0,0 +1,38 @@
+function pluck<DataType, KeyType extends keyof DataType>(
+  items: DataType[],
+  key: KeyType
+): DataType[KeyType][] {
+  return items.map((item) => item[key]);
+}
+
+const dogs = [
+  { name: "Mimi", age: 12 },
+  { name: "LG", age: 13 },
+];
+
+console.log(pluck(dogs, "age"));
+console.log(pluck(dogs, "name"));
+
+interface BaseEvent {
+  time: number;
+  user: string;
+}
+interface EventMap {
+  addToCart: BaseEvent & { quantity: number; productID: string };
+  checkout: BaseEvent;
+}
+
+function sendEvent<Name extends keyof EventMap>(
+  name: Name,
+  data: EventMap[Name]
+): void {
+  console.log([name, data]);
+}
+
+sendEvent("addToCart", {
+  productID: "foo",
+  user: "baz",
+  quantity: 1,
+  time: 10,
+});
+sendEvent("checkout", { time: 20, user: "bob" });
