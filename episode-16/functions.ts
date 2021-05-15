090250bfd06d38fa97cd8a3e0d86708ffcc493ea
Vibhanshu Chaturvedi <vibhanshu.chaturvedi@olx.com>
2021-05-15 11:29:56 +0530

    episode 16

---
+++ episode-16/functions.ts
@@ -0,0 +1,26 @@
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+export default addNumbers;
+
+export const addStrings = (str1: string, str2: string = ""): string =>
+  `${str1} ${str2}`;
+
+export const format = (title: string, param: string | number): string =>
+  `${title} ${param}`;
+
+export const printFormat = (title: string, param: string | number): void => {
+  console.log(format(title, param));
+};
+
+export const fetchData = (url: string): Promise<string> =>
+  Promise.resolve(`Data from ${url}`);
+
+function introduce(salutation: string, ...names: string[]): string {
+  return `${salutation} ${names.join(" ")}`;
+}
+
+export function getName(user: { first: string; last: string }): string {
+  return `${user?.first ?? "first"} ${user?.last ?? "last"}`;
+}
